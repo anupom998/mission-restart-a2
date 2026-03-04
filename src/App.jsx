7c1659be804d928869d5addad92933d9cbb161ad
anupom998 <anupom11118@gmail.com>
2026-03-04 12:56:58 +0600

    Initial commit: React + Vite + Taiwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      Hello Vite
+      <p className='text-3xl font-bold text-red-400'>Ok</p>
+      
+    </>
+  )
+}
+
+export default App
